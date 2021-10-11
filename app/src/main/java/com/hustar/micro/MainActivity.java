760847b4d5064c7f1feb058ba5bad87fe60d87aa
inhwanK <inhan1009@naver.com>
2021-10-11 18:54:08 +0900

    videoView 생성. 클릭하면 볼 수 있게 프로그래밍. 에뮬레이터로 테스트 안해본 상태

--- app/src/main/java/com/hustar/micro/MainActivity.java
+++ app/src/main/java/com/hustar/micro/MainActivity.java
@@ -2,14 +2,38 @@ package com.hustar.micro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-// 커밋 테스트
+    // 커밋 테스트
+    public static final String VIDEO_URL = "https://sites.google.com/site/ubiaccessmobile/sample_video.mp4";
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
+
+        // 유튜브 URL = https://www.youtube.com/watch?v=4Les0OrRuZ0
+
+        videoView = findViewById(R.id.videoView);
+
+        MediaController mc = new MediaController(this);
+        videoView.setMediaController(mc);
+
+        //Button button = findViewById(R.id.button);
+        videoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoView.setVideoURI(Uri.parse(VIDEO_URL));
+                videoView.requestFocus();
+                videoView.start();
+            }
+        });
     }
 }
